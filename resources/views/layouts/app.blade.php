f2da277faba1ce0b70dd69b579f2d1949aa39a09
abi30 <rakib0751@gmail.com>
2022-10-11 18:21:14 +0200

    initial master

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Library Management System') }}</title>
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+  
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"> <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }} "> <!-- Custom stlylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                        <li class="nav-item" ><a  class="nav-link " role="button" href="{{ route('admin.dashboard') }}">Dashboard</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('authors') }}">Authors</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('publishers') }}">Publishers</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('categories') }}">Categories</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('books') }}">Books</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('students') }}">Reg Students</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('book_issued') }}">Book Issue</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('reports') }}">Reports</a></li>
+                        <li class="nav-item" ><a  class="nav-link "  href="{{ route('settings') }}">Settings</a></li>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                      
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+     <!-- FOOTER -->
+     <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>© Copyright 2022 <a href="http://alrakib.codefactory.live/">Abi30😎</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous">
+    </script>
+     <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+     <script src="{{ asset('js/popper.min.js') }}"></script>
+     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
---
+++ resources/views/layoutss/app.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Library Management System') }}</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}"> <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }} "> <!-- Custom stlylesheet -->
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <div class="container">
+            @guest
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav me-auto">
+
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    {{-- @guest --}}
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Hi {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        <div id="menubar">
+                            <!-- Menu Bar -->
+                            
+                        </div>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div id="header">
+        <!-- HEADER -->
+        <div class="container">
+            <div class="row">
+                <div class="offset-md-4 col-md-4">
+                    <div class="logo">
+                        <a href="#"><img src="{{ asset('images/library.png') }}"></a>
+                    </div>
+                </div>
+                <div class="offset-md-2 col-md-2">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Hi {{ auth()->user()->name }}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="{{ route('change_password') }}">Change Password</a>
+                            <a class="dropdown-item" href="#" onclick="document.getElementById('logoutForm').submit()">Log Out</a>
+                        </div>
+                        <form method="post" id="logoutForm" action="{{ route('logout') }}">
+                            @csrf
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /HEADER -->
+   <div id="menubar">
+        <!-- Menu Bar -->
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <ul class="menu">
+                        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                        <li><a href="{{ route('authors') }}">Authors</a></li>
+                        <li><a href="{{ route('publishers') }}">Publishers</a></li>
+                        <li><a href="{{ route('categories') }}">Categories</a></li>
+                        <li><a href="{{ route('books') }}">Books</a></li>
+                        <li><a href="{{ route('students') }}">Reg Students</a></li>
+                        <li><a href="{{ route('book_issued') }}">Book Issue</a></li>
+                        <li><a href="{{ route('reports') }}">Reports</a></li>
+                        <li><a href="{{ route('settings') }}">Settings</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Menu Bar --> 
+
+    <div>
+     <main class="py-4">
+            @yield('content')
+        </main>
+
+    </div>
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <span>© Copyright 2022 <a href="http://alrakib.codefactory.live/">Abi30😎</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /FOOTER -->
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
